7dfdcac74c7da099f2688bc5bf7ec2b9242dc46c
Kidusr <kidusr69@gmail.com>
2022-09-08 17:18:25 -0700

    task 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main -print the string in the put function
+*
+* this program prints out "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
