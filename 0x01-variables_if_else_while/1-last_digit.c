6b6ac5fa23f242825f34fb8fc01c60f5fb5290b4
Kidusr <kidusr69@gmail.com>
2022-09-09 18:25:05 -0700

    task 0-9

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+include <stdio.h>
+/**
+*main - print if the number is postive , zero or negative
+*
+*Description: using the main function
+* this program prints "programming is postive, zero or negative
+*Return: 0
+*/
+int main(void)
+{
+int n:
+int l;
+
+srand(time(0));
+n = rand() -RAND_MAX / 2;
+i = n % 10;
+
+if (i > 5)
+{
+	printf("Last digit of %d is %d and is greater than s\n" , n , l);
+}
+else if (1 == 0)
+{
+	printf("Last digit of %d is %d and 0\n" , n , l);
+}
+else
+{
+	print("Last digit of %d is %d and is less than 6 and not 0\n , n , l);
+}
+return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*  main - print if the number is postive , zero or negative
+*
+*Description: using the main function
+* this programmprints "programming is postive , zero or negative
+*Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
