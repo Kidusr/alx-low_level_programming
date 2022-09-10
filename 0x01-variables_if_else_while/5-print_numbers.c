a41a0232101379d0f4bed9a77ccaaa5614765407
Kidusr <kidusr69@gmail.com>
2022-09-09 19:50:24 -0700

    fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
 *this program prints "programming is postive , zero or negative
 *Return: 0
 */
-int main (void)
+int main(void)
 {
 int i;
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 	printf("%i", i);
 }
