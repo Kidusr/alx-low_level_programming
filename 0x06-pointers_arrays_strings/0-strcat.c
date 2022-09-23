2640a702d0feabe956a4fbb580a674244573ddad
Kidusr <kidusr69@gmail.com>
2022-09-23 06:55:37 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,38 @@
  *
  * Return: A pointer to the distination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
+
 {
-	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	int dlen = 0, i;
 
-	for (index = 0; src[index]; index++)
-		dest(dest_len++) = src[index];
+
+
+	while (dest[dlen])
+
+	{
+
+		dlen++;
+
+									}
+
+
+
+	for (i = 0; src[i] != 0; i++)
+
+	{
+	
+		dest[dlen] = src[i];
+	
+		dlen++;
+
+	}
+
+
+
+	dest[dlen] = '\0';
 
 	return (dest);
+
 }
